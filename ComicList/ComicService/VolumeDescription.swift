12c8e76e55d5abda7b4ff503d5b079de6a881ec3
Michel Barbou Salvador <mibarbou@gmail.com>
2016-10-09 21:05:32 +0200

    Added an extension for String, to strip all html from the string value of the field description for the model VolumeDescription

--- ComicList/ComicService/VolumeDescription.swift
+++ ComicList/ComicService/VolumeDescription.swift
@@ -17,6 +17,28 @@ public struct VolumeDescription {
 extension VolumeDescription: JSONDecodable {
     
     public init?(dictionary: JSONDictionary) {
-        description = dictionary["description"] as? String
+        
+        guard let stringHtml = dictionary["description"] as? String else {
+            
+            return nil
+        }
+        
+        description = stringHtml.html2String
+        
+    }
+}
+
+extension String {
+    var html2AttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return  nil
+        }
+    }
+    var html2String: String? {
+        return html2AttributedString?.string ?? nil
     }
 }
